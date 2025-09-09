654cbc71e1d4bed791f4c9e6ad73f8daa0a47d49
aprem7369-glitch <a.prem7369@gmail.com>
2025-09-09 09:48:40 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SiteLabelsForm from './components/SiteLabelsForm';
+import Prem from './components/Prem';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SiteLabelsForm/>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
